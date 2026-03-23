c2c2ef49c1a5f19be431fb77e08e2918838f35c1
Yusuf Yalim <yusufyalim@gmail.com>
2026-03-24 00:11:38 +0100

    feat: update Next.js configuration and Prisma schema for improved output tracing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingIncludes: {
+    '/*': ['./generated/prisma/**/*'],
+  },
 };
 
 export default nextConfig;
